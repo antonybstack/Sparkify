a772f4fafa29e7bd3dfc2dc93875b00295e00eb4
antonybstack <antonyblyakher@gmail.com>
2023-08-14 22:19:16 -0400

    add additional http connection logging with custom template

--- Sparkify/Program.cs
+++ Sparkify/Program.cs
@@ -24,12 +24,20 @@ WebApplication app = builder.Build();
 
 app.UseSerilogRequestLogging(options =>
 {
+    options.MessageTemplate = "{RequestMethod} {Protocol} {RequestPath} responded {StatusCode} {ContentType} in {Elapsed:0.00} ms from {TraceIdentifier} {RemoteIpAddress}:{RemotePort}";
     options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
     {
         diagnosticContext.Set("Host", httpContext.Request.Host.Value);
         diagnosticContext.Set("Protocol", httpContext.Request.Protocol);
         diagnosticContext.Set("Scheme", httpContext.Request.Scheme);
         diagnosticContext.Set("QueryString", httpContext.Request.QueryString.Value);
+        diagnosticContext.Set("ContentType", httpContext.Request.ContentType);
+        diagnosticContext.Set("ContentLength", httpContext.Request.ContentLength);
+        diagnosticContext.Set("TraceIdentifier", httpContext.TraceIdentifier);
+        diagnosticContext.Set("RemoteIpAddress", httpContext.Connection.RemoteIpAddress);
+        diagnosticContext.Set("RemotePort", httpContext.Connection.RemotePort);
+        diagnosticContext.Set("LocalIpAddress", httpContext.Connection.LocalIpAddress);
+        diagnosticContext.Set("LocalPort", httpContext.Connection.LocalPort);
     };
 });
 
