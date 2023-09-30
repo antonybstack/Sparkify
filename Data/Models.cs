4c6f97e79a3ccf4e28aaa4ee4363a6c74908282e
antonybstack <antonyblyakher@gmail.com>
2023-09-30 18:50:48 -0400

    add blog related models

--- Data/Models.cs
+++ Data/Models.cs
@@ -1,35 +1,50 @@
-using System.ComponentModel;
 using System.Diagnostics;
 using System.Text.Json.Serialization;
 using Raven.Client.Documents.Changes;
 
-namespace Data;
+namespace Sparkify;
 
-public class User : IEntity
+public sealed class Blog : IEntity
 {
+    public string? Company { get; set; }
+    public string? Link { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    [DefaultValue(null)]
     public string Id { get; init; }
-    public string FirstName { get; init; }
-    public string LastName { get; init; }
 }
 
-public record PaymentEvent : IEvent
+public sealed record RssBlogFeed : IEntity
 {
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    [DefaultValue(null)]
     public string Id { get; init; }
-    [DefaultValue(1000)]
-    public int Amount { get; init; }
-    [DefaultValue(nameof(EventType.PaymentRequested))]
-    public EventType EventType { get; init; }
-    public string ReferenceId { get; init; }
+    public string BlogId { get; set; }
 }
 
-public interface IEvent : IEntity
+public sealed record Article : IEntity
 {
-    public EventType EventType { get; init; }
-    public string ReferenceId { get; init; } // database ID of the aggregate that the event belongs to
+    public string Id { get; init; }
+    public string BlogId { get; init; }
+    public string Link { get; set; }
+    public DateTimeOffset Date { get; set; }
+    public string? Title { get; set; }
+    public string? Subtitle { get; set; }
+    // public string? HtmlContent { get; set; }
+    public ICollection<string>? Authors { get; set; }
+    public ICollection<string>? Categories { get; set; }
+    public string Uid { get; set; }
+}
+
+public sealed record Payload<T>
+{
+    public T? Data { get; init; }
+    public RequestStatistics Stats { get; init; }
+}
+
+public sealed record RequestStatistics
+{
+    public long DurationInMs { get; init; }
+    public long TotalResults { get; init; }
 }
 
 public interface IEntity
@@ -37,50 +52,14 @@ public interface IEntity
     public string Id { get; init; }
 }
 
-public enum EventType
-{
-    Invalid,
-    PaymentRequested,
-    PaymentCompleted,
-    PaymentFailed,
-    PaymentCancelled,
-    PaymentRefunded,
-    PaymentExpired,
-    PaymentDeclined,
-    PaymentReversed,
-    PaymentSettled,
-    PaymentCaptured,
-    PaymentAuthorized,
-    PaymentVoided,
-    PaymentChargedBack,
-    PaymentChargebackReversed,
-    PaymentChargebackDeclined,
-    PaymentChargebackSettled,
-    PaymentChargebackExpired,
-    PaymentChargebackCancelled,
-    PaymentChargebackFailed,
-    PaymentChargebackRequested,
-    PaymentChargebackReversedRequested,
-    PaymentChargebackSettledRequested,
-    PaymentChargebackExpiredRequested,
-    PaymentChargebackCancelledRequested,
-    PaymentChargebackFailedRequested
-}
-
 public class IndexChangeObserver : IObserver<IndexChange>
 {
-    public void OnCompleted()
-    {
+    public void OnCompleted() =>
         Debug.WriteLine("All changes have been processed.");
-    }
 
-    public void OnError(Exception error)
-    {
-        // Handle any errors.
-    }
+    public void OnError(Exception error) =>
+        Debug.WriteLine($"Error: {error.Message}");
 
-    public void OnNext(IndexChange change)
-    {
+    public void OnNext(IndexChange change) =>
         Debug.WriteLine($"Index {change.Name} has changed. Type of change: {change.Type}");
-    }
 }
