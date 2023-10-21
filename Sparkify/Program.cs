ac179724c36d3374b792093a7ad4223e3ac2668e
antonybstack <antonyblyakher@gmail.com>
2023-10-21 19:47:04 -0400

    Update app startup configurations, add rate limiter and response caching services, add http client, forward headers

--- Sparkify/Program.cs
+++ Sparkify/Program.cs
@@ -1,18 +1,23 @@
+using System.Diagnostics;
+using System.Globalization;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Text.Json.Serialization;
+using System.Threading.RateLimiting;
 using Common.Configuration;
 using Common.Observability;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Hosting.Server.Features;
-using Microsoft.AspNetCore.Http.Json;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Serilog;
-using Sparkify.Features.Payment;
-using Sparkify.Observability;
+using Sparkify;
+using Sparkify.Features.BlogFeatures;
+using JsonOptions = Microsoft.AspNetCore.Http.Json.JsonOptions;
 
-WebApplicationBuilder builder = WebApplication.CreateSlimBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddWindowsService();
+
 ServicePointManager.DefaultConnectionLimit = 10000;
 
 var appOptions = builder.AddConfigAndValidate<ApiOptions, ValidateApiOptions>();
@@ -20,23 +25,45 @@ var databaseOptions = builder.AddConfigAndValidate<DatabaseOptions, ValidateData
 var otlpOptions = builder.AddConfigAndValidate<OtlpOptions, ValidateOtlpOptions>();
 
 builder.WebHost
-.UseQuic()
-.UseKestrel(options =>
-{
-    options.ListenLocalhost(6002, listenOptions =>
+    .UseQuic()
+    .UseKestrel((hostingContext, options) =>
     {
-        listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
-        listenOptions.UseHttps();
-        // https://learn.microsoft.com/en-us/aspnet/core/fundamentals/servers/kestrel/connection-middleware?view=aspnetcore-8.0
-        // listenOptions.UseConnectionLogging();
+        options.ListenAnyIP(appOptions.Port,
+            listenOptions =>
+            {
+                listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
+                listenOptions.UseHttps();
+                if (hostingContext.HostingEnvironment.IsDevelopment())
+                {
+                    listenOptions.UseConnectionLogging();
+                }
+            });
     });
-});
 
-builder.Services.AddHttpsRedirection(options => options.HttpsPort = 6002);
 builder.Services.AddHttpsRedirection(options => options.HttpsPort = appOptions.Port);
 
-builder.Services.AddCors(c => c.AddDefaultPolicy(policy => policy.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin()));
-builder.Services.Configure<JsonOptions>(options => options.SerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        if (!builder.Environment.IsProduction())
+        {
+            policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        }
+        else
+        {
+            policy.WithOrigins(appOptions.AllowedOrigins)
+                .AllowAnyHeader()
+                .WithMethods(HttpMethods.Get);
+        }
+    });
+});
+builder.Services.Configure<JsonOptions>(static options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+});
+
 builder.Services.AddControllers(options =>
 {
     options.RespectBrowserAcceptHeader = true;
@@ -47,23 +74,64 @@ builder.Services.AddControllers(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument();
 
-// builder.Services.AddSwaggerGen(c =>
-// {
-//     c.UseInlineDefinitionsForEnums();
-//     // c.AddMissingSchemas();
-// });
 builder.RegisterOpenTelemetry(otlpOptions);
 builder.RegisterSerilog(otlpOptions);
 /* DEPENDENCY INJECTION (SERVICES) SECTION */
-builder.Services.TryAddSingleton(DbManager.Store);
-builder.Services.TryAddSingleton<IEventChannel, EventChannel>();
-// builder.Services.AddSignalR();
-builder.Services.AddHostedService<SubscriptionWorker>();
+builder.Services.AddHttpClient<FaviconHttpClient>(static client =>
+{
+    client.DefaultRequestHeaders.UserAgent
+        .ParseAdd(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36");
+    client.DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrHigher;
+});
 
-WebApplication app = builder.Build();
+builder.Services.Configure<ForwardedHeadersOptions>(static options =>
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
+
+builder.Services.AddResponseCaching();
+
+builder.Services.AddRateLimiter(static options =>
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, IPAddress>(static context =>
+    {
+        var remoteIpAddress = context.Connection.RemoteIpAddress;
+        if (remoteIpAddress is null)
+        {
+            return RateLimitPartition.GetNoLimiter(IPAddress.Loopback);
+        }
+        if (!IPAddress.IsLoopback(remoteIpAddress!))
+        {
+            return RateLimitPartition.GetFixedWindowLimiter(remoteIpAddress,
+                static _ =>
+                    new FixedWindowRateLimiterOptions
+                    {
+                        PermitLimit = 1,
+                        AutoReplenishment = true,
+                        Window = TimeSpan.FromMilliseconds(100),
+                        QueueLimit = 5,
+                        QueueProcessingOrder = QueueProcessingOrder.NewestFirst
+                    });
+        }
+        return RateLimitPartition.GetNoLimiter(IPAddress.Loopback);
+    }));
 
 DbManager.CreateStore(databaseOptions.Name, databaseOptions.Http, databaseOptions.TcpHostName, databaseOptions.TcpPort);
+var app = builder.Build();
+
+CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+
+app.UseForwardedHeaders();
 app.UseHttpsRedirection();
+app.UseRouting();
+app.UseRateLimiter();
+app.UseCors();
+app.UseResponseCaching();
+
 app.RegisterSerilogRequestLogging();
 app.LogStartupInfo(builder);
 
@@ -79,67 +147,85 @@ else
     app.UseExceptionHandler("/Error");
 }
 
-const string htmlContent = """
-<!DOCTYPE html>
-<html lang=""en"">
-    <head>
-        <meta charset=""UTF-8"">
-        <title>Sparkify</title>
-        <link rel=\"icon\" href=\"data:,\">
-    </head>
-    <body>
-        <h1>Sparkify</h1>
-        <body style=\"background: rgb(43, 42, 51); color: #333;\">Hello Sparkify!</body>
-    </body>
-</html>
-""";
-app.MapGet("", (HttpContext context) =>
+if (app.Environment.IsDevelopment())
 {
-    context.Response.ContentType = "text/html";
-    return htmlContent;
-});
-app.UseSwagger(
-    c => { c.RouteTemplate = "api/{documentName}/swagger.json"; } // documentName is version number
-);
-app.UseSwaggerUI(c =>
-{
-    c.RoutePrefix = "api";
-    c.SwaggerEndpoint("v1/swagger.json", "Sparkify API v1");
-    c.DisplayRequestDuration();
-});
-app.MapGet("api/systeminfo", async context =>
-{
-    var systemInfo = new
-    {
-        RuntimeInformation.OSDescription,
-        RuntimeInformation.OSArchitecture,
-        RuntimeInformation.ProcessArchitecture,
-        Environment.ProcessorCount,
-        Environment.SystemPageSize
-    };
+    const string HtmlContent = """
+                               <!DOCTYPE html>
+                               <html lang=""en"">
+                                   <head>
+                                       <title>Sparkify</title>
+                                       <link rel=\"icon\" href=\"data:,\">
+                                   </head>
+                                   <body>
+                                       <h1>Sparkify</h1>
+                                       <body style=\"background: rgb(43, 42, 51); color: #333;\">Hello Sparkify!</body>
+                                   </body>
+                               </html>
+                               """;
+    app.MapGet("",
+        static (HttpContext context) =>
+        {
+            context.Response.ContentType = "text/html";
+            return HtmlContent;
+        });
 
-    await context.Response.WriteAsJsonAsync(systemInfo);
-});
-app.MapPaymentApi();
-app.Map("error", async context =>
-    await context.Response.WriteAsync(
-        "An error occurred. The server encountered an error and could not complete your request.")
+    app.MapGet("api/systeminfo",
+        async static (HttpContext context, ILogger<Program> logger) =>
+        {
+            var systemInfo = new
+            {
+                RuntimeInformation.OSDescription,
+                RuntimeInformation.OSArchitecture,
+                RuntimeInformation.ProcessArchitecture,
+                Environment.ProcessorCount,
+                Environment.SystemPageSize
+            };
+            logger.LogInformation("systeminfo executed!");
+            logger.LogTrace("systeminfo executed trace!");
+
+            using var activity =
+                Common.Observability.OpenTelemetry.Config.ActivitySource.StartActivity("SystemInfoActivity",
+                    ActivityKind.Server);
+            activity?.SetTag("foo", 1);
+            activity?.SetTag("bar", "Hello, World!");
+            activity?.SetTag("baz",
+                new[]
+                {
+                    1, 2, 3
+                });
+
+            await context.Response.WriteAsJsonAsync(systemInfo);
+        });
+}
+
+app.MapBlogsApi();
+
+app.Map("error",
+    static context =>
+        context.Response.WriteAsync(
+            "An error occurred. The server encountered an error and could not complete your request.")
 );
-app.MapFallback(async context => { await context.Response.WriteAsync("Page not found"); });
-// log all endpoints
+app.MapFallback(static async context => { await context.Response.WriteAsync("Page not found"); });
+
+// log all endpoint paths
 app.Lifetime.ApplicationStarted.Register(() =>
 {
-    ILogger<Program> logger = app.Services.GetRequiredService<ILogger<Program>>();
-    using IServiceScope scope = app.Services.CreateScope();
-    EndpointDataSource dataSource = scope.ServiceProvider.GetRequiredService<EndpointDataSource>();
-    IServer kestrelServer = scope.ServiceProvider.GetRequiredService<IServer>();
+    using var scope = app.Services.CreateScope();
+    var dataSource = scope.ServiceProvider.GetRequiredService<EndpointDataSource>();
+    var kestrelServer = scope.ServiceProvider.GetRequiredService<IServer>();
     var baseUrl = kestrelServer.Features.Get<IServerAddressesFeature>()?.Addresses.First();
-    logger.LogInformation("Open API: {Route}/api", baseUrl);
-    foreach (Endpoint endpoint in dataSource.Endpoints)
+    app.Logger.LogInformation("Open API: {Route}/api", baseUrl);
+    foreach (var endpoint in dataSource.Endpoints)
     {
-        if (endpoint is not RouteEndpoint routeEndpoint) continue;
-        logger.LogInformation("{Route}/{RawText} : {DisplayName}", baseUrl, routeEndpoint.RoutePattern.RawText, routeEndpoint.DisplayName);
+        if (endpoint is not RouteEndpoint routeEndpoint)
+        {
+            continue;
+        }
+        app.Logger.LogInformation("{Route}/{RawText} : {DisplayName}",
+            baseUrl,
+            routeEndpoint.RoutePattern.RawText,
+            routeEndpoint.DisplayName);
     }
 });
-app.UseCors();
+
 app.Run();
